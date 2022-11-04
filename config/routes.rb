d0254509009f104388ea9022103bd4efdb9fcdd8
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-03 18:40:14 -0600

    No changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # feel free to remove or change or delete this resource; ask MM if you have any questions about it
-  
+
   resources :merchants, only: [:index]
-  
+
   resources :merchants do
     resources :items, only: [:index, :show, :edit, :update]
   end
-  
+
   # get "/admin", to: "admin#index"
   resources :admin, only: [:index]
 
