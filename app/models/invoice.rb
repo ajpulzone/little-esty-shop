2e27aa129227b4a8b04526f8002bbe63348f8860
Annie Pulzone <ajpulzone@gmail.com>
2022-11-04 21:02:39 -0700

    Add incomplete_invoices model method and associated testing

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,7 +8,9 @@ class Invoice < ApplicationRecord
     created_at.strftime('%A, %B%e, %Y')
   end
   
-
+  def self.incomplete_invoices
+    self.joins(:invoice_items).where.not(invoice_items: {status: 2}).distinct.order(:created_at)
+  end
 
 
 end
