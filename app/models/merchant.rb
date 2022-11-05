c479bf70c92c03be6ab73a0ee8dff78606b93f3a
Rich Kaht <adamselene1776@protonmail.com>
2022-11-05 13:58:07 -0600

    Feat/Test: merchant_top_5_customers method for Merchant Model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,9 +1,8 @@
 class Merchant < ApplicationRecord
   has_many :items
-
   has_many :invoice_items, through: :items
-
   has_many :invoices, through: :items
+  has_many :customers, through: :invoices
 
   def unique_invoices
     invoices.distinct
@@ -11,6 +10,10 @@ class Merchant < ApplicationRecord
   
   def invoices_not_shipped
     invoice_items.where(status: ['0', '1'])
+  end
+  
+  def merchant_top_5_customers
+    customers.select(:id, :first_name, :last_name, 'count(transactions.*) as number_transactions').joins(:transactions).where('transactions.result =?','success').group(:id).order('number_transactions desc').limit(5)
   end
 
 end
