f833e5bcb4a088e839e20960f9f29df25de79644
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-05 20:35:36 -0600

    Begin querying complicated stuff

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -14,9 +14,13 @@ class Merchant < ApplicationRecord
   def invoice_revenue(invoice_id)
     items_for_this_invoice(invoice_id).sum('invoice_items.unit_price * invoice_items.quantity')
   end
-  
+
   def invoices_not_shipped
     invoice_items.where(status: ['0', '1'])
+  end
+
+  def top_five
+    find_by_sql("select merchants.name, invoices.id as invoice_id, result as result_of_transaction, invoice_items.quantity*invoice_items.unit_price as revenue, invoice_items.quantity, invoice_items.unit_price, items.name as item from items on merchants.id = items.merchant_id join invoice_items on invoice_items.item_id = items.id join invoices on invoice_items.id = invoices.id join transactions on transactions.invoice_id = invoices.id where result = 'success' and merchants.id = 1 order by revenue desc limit 5")
   end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,8 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+  # possible fix for the Heroku deployment; uncomment line 28 and attempt to push to Heroku
+  # config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
