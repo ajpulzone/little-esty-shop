74ce8f63423803df13707642d00dd4ff479eb701
Annie Pulzone <ajpulzone@gmail.com>
2022-11-05 18:18:23 -0700

    Added enum validations

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -50,6 +50,7 @@ RSpec.describe Merchant do
       it 'returns the total revenue for items sold on this invoice' do
         expect(@merchant1.invoice_revenue(@invoice1.id)).to eq(5400)
       end
+    end 
     
     describe '#invoices_not_shipped' do
       it "returns a list of items for invoices that are either 'packaged' or 'pending'" do
