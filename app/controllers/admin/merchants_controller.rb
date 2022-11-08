31c49be59041ca40cb989904a1e583b91af69376
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-07 17:29:17 -0700

    Updates the controller and uses strong params to update merchant

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -1,5 +1,4 @@
 class Admin::MerchantsController < ApplicationController
-  
   def index
     @merchants = Merchant.all
   end
@@ -8,11 +7,24 @@ class Admin::MerchantsController < ApplicationController
     @merchant = Merchant.find(params[:id])
   end
 
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end
+
   def update
     @merchant = Merchant.find(params[:id])
     if params[:status].present?
       @merchant.update(status: params[:status])
       redirect_to admin_merchants_path
+    elsif params.has_key?('merchant')
+      @merchant.update(update_params)
+      redirect_to admin_merchant_path(@merchant.id)
     end
+  end
+
+  private
+
+  def update_params
+    params.require(:merchant).permit(:name)
   end
 end
