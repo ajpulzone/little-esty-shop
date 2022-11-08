c7f2f14e4f90eb9664ecc330190d2fdaadeb3b51
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-11-08 15:52:08 -0700

    Adds a URI call

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,13 +1,13 @@
-require 'json'
 
 class MerchantsController < ApplicationController
+  require 'json'
   require 'net/https'
-  def index
-  end
+  
+  def index; end
 
   def show
     @merchant = Merchant.find(params[:id])
-      uri = URI("https://api.github.com/users/freeing3092")
+    uri = URI('https://api.github.com/users/freeing3092')
     @data = Net::HTTP.get(uri)
     @data = JSON.parse(@data)
   end
