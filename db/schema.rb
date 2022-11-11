952a07293854cae77cd9382fe2e44a679757cef6
Annie Pulzone <ajpulzone@gmail.com>
2022-11-11 08:58:12 -0800

    Feat: create bulk discounts and bulk discount item tables, migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_05_212031) do
+ActiveRecord::Schema.define(version: 2022_11_11_162645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bulk_discount_items", force: :cascade do |t|
+    t.bigint "bulk_discount_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bulk_discount_id"], name: "index_bulk_discount_items_on_bulk_discount_id"
+    t.index ["item_id"], name: "index_bulk_discount_items_on_item_id"
+  end
+
+  create_table "bulk_discounts", force: :cascade do |t|
+    t.bigint "merchant_id"
+    t.float "discount_percent"
+    t.integer "quantity_threshold"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["merchant_id"], name: "index_bulk_discounts_on_merchant_id"
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
@@ -70,6 +88,9 @@ ActiveRecord::Schema.define(version: 2022_11_05_212031) do
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
+  add_foreign_key "bulk_discount_items", "bulk_discounts"
+  add_foreign_key "bulk_discount_items", "items"
+  add_foreign_key "bulk_discounts", "merchants"
   add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "customers"
