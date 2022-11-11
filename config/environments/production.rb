cfef0408fad36e938b4ce6c07037b39a920b233f
Annie Pulzone <ajpulzone@gmail.com>
2022-11-10 19:10:35 -0800

    Trouble shooting Heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,9 +24,7 @@ Rails.application.configure do
   config.assets.js_compressor = Uglifier.new(harmony: true)
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
-  # possible fix for the Heroku deployment; uncomment line 28 and attempt to push to Heroku
-  # config.assets.js_compressor = Uglifier.new(harmony: true)
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
