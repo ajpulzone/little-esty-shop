3fe33fe79181640363190a8d1769adbfbabd63fe
Annie Pulzone <ajpulzone@gmail.com>
2022-11-13 19:19:11 -0800

    WIP API's

---
+++ app/services/nager_services.rb
@@ -0,0 +1,12 @@
+require "httparty"
+
+# class NagerService
+#   def repo_name
+#     get_url("https://date.nager.at/api/v3/NextPublicHolidays/US")
+#   end
+
+#   def get_url(url)
+#     response = HTTParty.get(url)
+#     JSON.parse(response.body, symbolize_names: true)
+#   end
+# end
