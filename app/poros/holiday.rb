a90b565ea5ecf318f0226585f126d385eafb0662
Annie Pulzone <ajpulzone@gmail.com>
2022-11-15 14:21:50 -0800

    WIP: API's

--- app/poros/holiday.rb
+++ app/poros/holiday.rb
@@ -1,8 +1,8 @@
-# class Holiday
-#   attr_reader :name, :date
+class Holiday
+  attr_reader :name, :date
 
-#   def initialize(data)
-#     @name = data[:name]
-#     @date = data[:date]
-#   end
-# end
+  def initialize(data)
+    @name = data[:name]
+    @date = data[:date]
+  end
+end
--- app/poros/holiday_search.rb
+++ app/poros/holiday_search.rb
@@ -1,31 +1,33 @@
 require "./app/poros/holiday"
 require "./app/services/nager_services.rb"
 
-# class HolidaySearch
+class HolidaySearch
   
-#   def next_3_holidays
+  def next_3_holidays
+    data = service.holiday[:name, :date]
+    Holiday.new(data)
 
-#   end
+  end
 
 
 
-#   def repo_information
-#     data = service.repo_name[:name]
-#     Repo.new(data)
-#   end
+  def repo_information
+    data = service.repo_name[:name]
+    Repo.new(data)
+  end
 
-#   def service
-#     GithubService.new
-#   end
+  def service
+    GithubService.new
+  end
 
-#   def contributor_names
-#     service.contributors.map do |data|
-#       Contributor.new(data)
-#     end
-#   end
+  def contributor_names
+    service.contributors.map do |data|
+      Contributor.new(data)
+    end
+  end
 
-#   def pull_requests
-#     data = service.pull_requests[0]
-#     latest_pull = PullRequest.new(data)
-#   end
-# end
+  def pull_requests
+    data = service.pull_requests[0]
+    latest_pull = PullRequest.new(data)
+  end
+end
